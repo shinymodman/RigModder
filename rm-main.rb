295f780b51a72e20ddb8b81fc8e005b466b78ce7
Billy Irvine <numberoneyoshifan@yahoo.com>
2022-04-26 20:35:29 -0400

    Add beam dialog and move menu items.

--- rm-main.rb
+++ rm-main.rb
@@ -40,39 +40,50 @@ class RMApp < Gtk::Application
 			
 			@view_menu = Gtk::Menu.new()
 			@view_sel = Gtk::MenuItem.new(:label => "View")
-		    @view_sel.set_submenu(@view_menu)
-		    # This adds the View dropdown to the top menu.
-		    
-		    
-		    @front = Gtk::MenuItem.new(:label => "Front")
-		    @view_menu.append(@front)
-		    # Creates and adds the selection saying front to the View dropdown.
-		    
-		    @back = Gtk::MenuItem.new(:label => "Back")
-		    @view_menu.append(@back)
-		    # Creates and adds the selection saying back to the View dropdown.
-		    
-		    @left = Gtk::MenuItem.new(:label => "Left")
-		    @view_menu.append(@left)
-		    # Creates and adds the selection saying left to the View dropdown.
-		    
-		    @right = Gtk::MenuItem.new(:label => "Right")
-		    @view_menu.append(@right)
-		    # Creates and adds the selection saying right to the View dropdown.
-		    
-		    @top = Gtk::MenuItem.new(:label => "Top")
-		    @view_menu.append(@top)
-		    # Creates and adds the selection saying top to the View dropdown.
-		    
-		    @bottom = Gtk::MenuItem.new(:label => "Bottom")
-		    @view_menu.append(@bottom)
-		    # Creates and adds the selection saying bottom to the View dropdown.
-	    	
-	    	@node_dialog = Gtk::MenuItem.new(:label => "Node List")
+			@view_sel.set_submenu(@view_menu)
+
+
+	    	@node_dialog = Gtk::MenuItem.new(:label => "Nodes")
 	    	@view_menu.append(@node_dialog)
 	    	# Creates a way to view dialog with a node list
 
+	    	@beam_dialog = Gtk::MenuItem.new(:label => "Beams")
+	    	@view_menu.append(@beam_dialog)
+	    	#Creates a way to view dialog with a beam list
+
 			@menu.append(@view_sel)
+			# Adds whole view menu to the whole user interface.
+
+			@camera_menu = Gtk::Menu.new()
+			@camera_sel = Gtk::MenuItem.new(:label => "Camera")
+		    @camera_sel.set_submenu(@camera_menu)
+		    # This adds the View dropdown to the top menu.
+		    
+		    @front = Gtk::MenuItem.new(:label => "Front")
+		    @camera_menu.append(@front)
+		    # Creates and adds the selection saying front to the View dropdown.
+		    
+		    @back = Gtk::MenuItem.new(:label => "Back")
+		    @camera_menu.append(@back)
+		    # Creates and adds the selection saying back to the View dropdown.
+		    
+		    @left = Gtk::MenuItem.new(:label => "Left")
+		    @camera_menu.append(@left)
+		    # Creates and adds the selection saying left to the View dropdown.
+		    
+		    @right = Gtk::MenuItem.new(:label => "Right")
+		    @camera_menu.append(@right)
+		    # Creates and adds the selection saying right to the View dropdown.
+		    
+		    @top = Gtk::MenuItem.new(:label => "Top")
+		    @camera_menu.append(@top)
+		    # Creates and adds the selection saying top to the View dropdown.
+		    
+		    @bottom = Gtk::MenuItem.new(:label => "Bottom")
+		    @camera_menu.append(@bottom)
+		    # Creates and adds the selection saying bottom to the View dropdown.
+	    	
+			@menu.append(@camera_sel)
 			@grid.add(@menu)
 			# Creates and inserts the dropdown menu widget to the window.
 			
@@ -176,7 +187,52 @@ class RMApp < Gtk::Application
 			@dialog_grid.add(@node_property_opt_grid)
 			# Places widgets and their placeholder to show widgets properly.
 
+			@beam_window = Gtk::Window.new()
+			@beam_window.set_title("Beams")
+			@beam_window.type_hint = :dialog
+
+			@beam_list = Gtk::ListBox.new()
+			@beam_list.expand = true
+
+			@beam_container = Gtk::Box.new(:vertical, 2)
+			@beam_scrollbar = Gtk::ScrolledWindow.new()
+
+			@beam_property_opt_grid = Gtk::Box.new(:horizontal)
+			@beam_property_id1_grid = Gtk::Box.new(:horizontal)
+			@beam_property_id2_grid = Gtk::Box.new(:horizontal)
+			# Grids to place node property widgets in its respecitve areas.
+
+			@label_beam_id1 = Gtk::Label.new()
+			@beam_id1_entry = Gtk::Entry.new()
+			@label_beam_id1.set_text("Node 1:")
+			@beam_property_id1_grid.add(@label_beam_id1)
+			@beam_property_id1_grid.pack_end(@beam_id1_entry, :expand => true, :fill => true, :padding => 0)
+			# Sets up widgets for showing primary node id data.
+
+			@label_beam_id2 = Gtk::Label.new()
+			@beam_id2_entry = Gtk::Entry.new()
+			@label_beam_id2.set_text("Node 2:")
+			@beam_property_id2_grid.add(@label_beam_id2)
+			@beam_property_id2_grid.pack_end(@beam_id2_entry, :expand => true, :fill => true, :padding => 0)
+			# Sets up widgets for showing secondary node id data.
+
+			@label_beam_opt = Gtk::Label.new()
+			@beam_opt_entry = Gtk::Entry.new()
+			@label_beam_opt.set_text("Opt:")			
+			@beam_property_opt_grid.add(@label_beam_opt)
+			@beam_property_opt_grid.pack_end(@beam_opt_entry, :expand => true, :fill => true, :padding => 0)
+			# Sets up widgets for showing node option data.
+
+			@beam_scrollbar.add(@beam_list)
+			@beam_container.add(@beam_scrollbar)
+
+			@beam_container.add(@beam_property_id1_grid)
+			@beam_container.add(@beam_property_id2_grid)
+			@beam_container.add(@beam_property_opt_grid)
+			@beam_window.add(@beam_container)
+
 			EVENT_FOR_STRUCTURE.load_dialog(@node_dialog, @window, 800, 350, @dialog_grid)
+			EVENT_FOR_STRUCTURE.load_dialog(@beam_dialog, @beam_window, 800, 350, @beam_container)
 			LOAD_TRUCK_FILE.load_content(@open_item, @canvas, @node_list, @node_entry, @node_x_entry,
 				@node_y_entry, @node_z_entry, @node_opt_entry)
 			DRAW_STRUCTURE.set_node_selector(@node_list)
