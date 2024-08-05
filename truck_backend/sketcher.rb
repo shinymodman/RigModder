b74934f3d6a3a49bf23d3a65b7de0a9b0e2734c1
unknown <numberoneyoshifan@yahoo.com>
2024-08-05 17:57:41 -0400

    Tweak how flare coords position.

--- truck_backend/sketcher.rb
+++ truck_backend/sketcher.rb
@@ -127,18 +127,18 @@ module DRAW_STRUCTURE
         x_placehold_counter = Node.new(trk, truck_flare_counter.get_reference_x)
         y_placehold_counter = Node.new(trk, truck_flare_counter.get_reference_y)
 
-        flare_arr[i] = Matrix[[node_placehold_counter.show_x],
-                              [node_placehold_counter.show_y + truck_flare_counter.get_coord_y],
-                              [node_placehold_counter.show_z - truck_flare_counter.get_coord_x]]
+        flare_arr[i] = Matrix[[(node_placehold_counter.show_x + node_placehold_counter.show_z) / 2],
+                              [(node_placehold_counter.show_y + truck_flare_counter.get_coord_y) / 2],
+                              [(node_placehold_counter.show_z + truck_flare_counter.get_coord_x) / 2]]
         # This matrix stores coords from the Reference Node
 
-        x_arr[i] = Matrix[[x_placehold_counter.show_x],
-                          [x_placehold_counter.show_y],
-                          [x_placehold_counter.show_z]]
+        x_arr[i] = Matrix[[(x_placehold_counter.show_x + x_placehold_counter.show_z) / 2],
+                          [(x_placehold_counter.show_y + truck_flare_counter.get_coord_y) / 2],
+                          [(x_placehold_counter.show_z + truck_flare_counter.get_coord_x) / 2]]
 
-        y_arr[i] = Matrix[[y_placehold_counter.show_x],
-                          [y_placehold_counter.show_y],
-                          [y_placehold_counter.show_z]]
+        y_arr[i] = Matrix[[(y_placehold_counter.show_x + y_placehold_counter.show_z) / 2],
+                          [(y_placehold_counter.show_y + truck_flare_counter.get_coord_y) / 2],
+                          [(y_placehold_counter.show_z + truck_flare_counter.get_coord_x) / 2]]
 
         y_arr[i] = flare_arr[i] + x_arr[i]
 
@@ -146,7 +146,7 @@ module DRAW_STRUCTURE
 
         @flare_matrix[i] = Matrix[[flare_arr[i][0,0] + x_arr[i][0,0]], 
                                   [flare_arr[i][1,0] + y_arr[i][1,0]], 
-                                  [flare_arr[i][2,0]]]
+                                  [flare_arr[i][2,0] + x_arr[i][2,0]]]
 
         i = i + 1
       end
@@ -290,7 +290,7 @@ module DRAW_STRUCTURE
 
           projected_2d = proj_mat * rotated_flare
 
-          b.rectangle(-projected_2d[0, 0] * (@size_for_flares), projected_2d[1, 0] * (@size_for_flares), 10, 10)
+          b.rectangle(-projected_2d[0, 0] * @size_for_flares, projected_2d[1, 0] * @size_for_flares, 10, 10)
         }
 
         b.fill()
