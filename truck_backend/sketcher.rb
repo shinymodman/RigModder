f5f1a91ec5e234c84eeb9abd0e58dc2d0ed0ffc6
unknown <numberoneyoshifan@yahoo.com>
2024-08-06 17:22:28 -0400

    Edit flare placement calculation (Revision 3).

--- truck_backend/sketcher.rb
+++ truck_backend/sketcher.rb
@@ -128,17 +128,21 @@ module DRAW_STRUCTURE
         y_placehold_counter = Node.new(trk, truck_flare_counter.get_reference_y)
 
         flare_arr[i] = Matrix[[node_placehold_counter.show_x],
-                              [node_placehold_counter.show_y + truck_flare_counter.get_coord_y],
-                              [node_placehold_counter.show_z - truck_flare_counter.get_coord_x]]
+                              [((EVENT_FOR_STRUCTURE::get_size_for_flares() * node_placehold_counter.show_y) + 
+                                (EVENT_FOR_STRUCTURE::get_size_for_flares() * truck_flare_counter.get_coord_y) / 2)],
+                              [((EVENT_FOR_STRUCTURE::get_size_for_flares() * node_placehold_counter.show_z) + 
+                                (EVENT_FOR_STRUCTURE::get_size_for_flares() * truck_flare_counter.get_coord_x) / 2)]]
         # This matrix stores coords from the Reference Node
 
         x_arr[i] = Matrix[[x_placehold_counter.show_x],
                           [x_placehold_counter.show_y],
-                          [x_placehold_counter.show_z]]
+                          [((EVENT_FOR_STRUCTURE::get_size_for_flares() * node_placehold_counter.show_z) + 
+                                (EVENT_FOR_STRUCTURE::get_size_for_flares() * truck_flare_counter.get_coord_x) / 2)]]
 
         y_arr[i] = Matrix[[y_placehold_counter.show_x],
                           [y_placehold_counter.show_y],
-                          [y_placehold_counter.show_z]]
+                          [((EVENT_FOR_STRUCTURE::get_size_for_flares() * node_placehold_counter.show_z) + 
+                                (EVENT_FOR_STRUCTURE::get_size_for_flares() * truck_flare_counter.get_coord_x) / 2)]]
 
         y_arr[i] = flare_arr[i] + x_arr[i]
 
@@ -290,7 +294,7 @@ module DRAW_STRUCTURE
 
           projected_2d = proj_mat * rotated_flare
 
-          b.rectangle(-projected_2d[0, 0] * (@size_for_flares), projected_2d[1, 0] * (@size_for_flares), 10, 10)
+          b.rectangle(-projected_2d[0, 0], projected_2d[1, 0], 10, 10)
         }
 
         b.fill()
